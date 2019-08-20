1bc9e7c9a8f797e415e0c2cc2eac003ffcbadbac
Jesse Gray <jessegray13@hotmail.com>
2019-08-20 14:58:42 +1200

    Wrote first code

--- src/main/java/nz/ac/eit/App.java
+++ src/main/java/nz/ac/eit/App.java
@@ -1,5 +1,7 @@
 package nz.ac.eit;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "What is the number to translate?" );
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine();
+        int intUI = Integer.parseInt(userInput);
+        if(intUI == 69){
+            System.out.println("Nice");
+        } else{
+            System.out.println("The number to translate is: " + intUI);
+        }
     }
 }
